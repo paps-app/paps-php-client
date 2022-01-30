74fabb8246b3973f018e1a5ebedc1c571277ad1f
kiamet <kiamet@paps-app.com>
2022-01-30 16:32:29 +0000

    test url

--- src/Paps/Resources/Delivery.php
+++ src/Paps/Resources/Delivery.php
@@ -131,7 +131,7 @@ class Delivery extends AbstractResource
      */
     public function submitQuotesRequest(array $quotes_params = [])
     {
-        return $this->setEndpoint($this->getEndpoint() . '/marketplace/getRate')
+        return $this->setEndpoint($this->getEndpoint() . 'marketplace')
             ->setParams($quotes_params)
             ->setMethod('GET')
             ->send();
