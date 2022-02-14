bbf519ecbf065cdb3f7a83d521a0497213c399b6
kiamet <kiamet@paps-app.com>
2022-02-14 08:41:29 +0000

    changed uri

--- src/Paps/PapsClient.php
+++ src/Paps/PapsClient.php
@@ -9,7 +9,7 @@ class PapsClient
     /**
      * API Base URL
      */
-    const API_BASE_PATH = 'url api';
+    const API_BASE_PATH = 'https://api.papslogistics.com/';
 
     /**
      * API Version
